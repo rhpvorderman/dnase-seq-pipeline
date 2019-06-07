292e4f07929ac1c95bfd8969381f87cbc47b2d0f
Ulugbek Baymuradov <ulugbekbk@gmail.com>
2019-06-06 17:05:43 -0700

    fixes
    Former-commit-id: bfbc72c36f74c1eb629930942f01f0ad8e3a421b
    Former-commit-id: 97e4689f4abb2094385d2f6c4516f02ec0218f22

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -36,7 +36,7 @@ function cmp_bam() {
     return 1
   fi
   echo "Comparing $expected with $output"
-  cmp <(samtools view "$expected") <(samtools view "$output") \
+  cmp <(samtools view $expected) <(samtools view $output) \
     || (echo "bams don't match" ; false)
 
 }
